afcebd45f6dba2a23c3bd8016192230d48548280
kamakshikumari <kamakshi.kumari@successive.tech>
2020-10-26 18:38:32 +0530

    Setup of Express

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// console.log("hi");
-const patterns_1 = require("./patterns");
-patterns_1.diamond(6);
-patterns_1.equilateral(6);
-const utils_1 = require("./utils");
-const constants_1 = require("./constants");
-let result = utils_1.hasPermission(constants_1.permissions.getUsers, 'head-trainer', 'Delete');
-console.log(result);
-utils_1.validateUsers(constants_1.user);
+const Server_1 = require("./Server");
+const configuration_1 = require("./config/configuration");
+console.log('config is', configuration_1.default);
+const server = new Server_1.default({ PORT: 9000 });
+server.bootstrap().run();
 //# sourceMappingURL=index.js.map
--- extraTs/patterns/equilateral.ts
+++ extraTs/patterns/equilateral.ts
@@ -1,23 +1,19 @@
-export default function equilateral(n:number)
-{
-  let str:string;
-  for(let i:number=1; i<=n; i++)
-  {
+export default function equilateral(n: number) {
+  let str: string;
+  for (let i: number = 1; i <= n; i++) {
     str = '';
-    for(let k:number=1; k<=n-i; k++)
-    {
-      str += " ";
+    for (let k: number = 1; k <= n - i ; k++) {
+      str += ' ';
     }
-    for(let j:number=1; j<=i; j++)
-    {
-      str += "* ";
+    for (let j: number = 1; j <= i; j++) {
+      str += '* ';
     }
     console.log(str);
-    str = "";
+    str = '';
   }
 
 }
 
-//let y=process.argv[2];
+// let y=process.argv[2];
 
-//equilateral(y);
+// equilateral(y);
