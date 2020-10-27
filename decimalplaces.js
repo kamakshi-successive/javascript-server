abbe73e7819d543e3c17b49776ffd113694104c7
kamakshikumari <kamakshi.kumari@successive.tech>
2020-10-27 13:53:14 +0530

    Skeleton Fix

--- decimalplaces.js
+++
@@ -1,2 +0,0 @@
-const decimal=2.100212;
-console.log(decimal.toFixed(2));
--- reverseastr.js
+++
@@ -1,9 +0,0 @@
-function rev(str)
-{
-    var splitstr=str.split("");
-    var revstr=splitstr.reverse();
-    var joinarray=revstr.join("");
-    console.log(joinarray);
-}
-
-rev("hello")
