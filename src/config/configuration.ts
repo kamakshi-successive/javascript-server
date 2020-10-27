046aa757ff7856d5101e8d03065093ac313bb531
kamakshikumari <kamakshi.kumari@successive.tech>
2020-10-27 19:52:07 +0530

    Upgrade Files

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,12 +1,21 @@
 import { IConfig } from './IConfig';
+import { config } from 'dotenv';
+
 // import * as dotenv from 'dotenv';
 // console.log(dotenv);
-
+config();
 
 // tslint:disable-next-line: no-var-requires
-const envVars = require ('dotenv').config();
-console.log('Inside config', envVars);
-const config = envVars.parsed;
-Object.freeze(config);
+// const envVars = require ('dotenv').config();
+const envVars = process.env;
+// console.log('Inside config', envVars);
+const configuration = Object.freeze({
+  port : envVars.PORT,
+  nodeEnv : envVars.NODE_ENV
+});
+
+// Object.freeze(configuration);
 // config.PORT=7000;
-export default config;
+export default configuration;
+
+// const ram = {name: 'Ram',};
