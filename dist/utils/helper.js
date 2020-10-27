5167e8dcbbc55fc5cc073a0ea3c0fe2eb03bfb36
kamakshikumari <kamakshi.kumari@successive.tech>
2020-10-27 17:09:25 +0530

    Configure

--- dist/utils/helper.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function validateEmail(Email) {
-    const RegExp = "^[a-zA-Z0-9+_.-]+@successive.tech+$";
-    let str = Boolean(Email.match(RegExp));
-    return str;
-}
-exports.default = validateEmail;
-//# sourceMappingURL=helper.js.map
