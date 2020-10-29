12c3b78a18ec4e6b38b508a77be54a18195282a6
kamakshikumari <kamakshi.kumari@successive.tech>
2020-10-29 12:46:50 +0530

    Modified

--- extraTs/index.ts
+++ extraTs/index.ts
@@ -1,15 +1,8 @@
 // console.log("hi");
-<<<<<<< HEAD
 import { diamond, equilateral } from './patterns';
 diamond(6);
 // console.log('from now equilateral print');
 equilateral(6);
-=======
-import { diamond,equilateral } from './patterns';
-diamond(6)
-console.log("Equilateral Progra#############@@@@@@@@@@@@@@");
-equilateral(6)
->>>>>>> eec50f962244033eec552b69b79ef65e47377a05
 
 import { hasPermission, validateUsers } from './utils';
 
