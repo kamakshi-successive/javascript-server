71aa288959b7f0a1b5a0af7470bee8819b42b01a
kamakshikumari <kamakshi.kumari@successive.tech>
2020-11-02 12:57:37 +0530

    Changes in files

--- src/controller/trainee/routes.ts
+++ src/controller/trainee/routes.ts
@@ -1,11 +1,10 @@
 import { Router } from  'express';
 import  TraineeController  from './Controller';
-import validationHandler from '../../../libs/validationHandler';
-import validation from './validation';
+
 const traineeRouter = Router();
 
 traineeRouter.route('/')
-  .get(validationHandler(validation.get), TraineeController.get)
+  .get(TraineeController.get)
   .post(TraineeController.create)
   .put(TraineeController.update)
   .delete(TraineeController.delete);
