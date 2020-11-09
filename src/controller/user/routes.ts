70a7618280398a0702398cefd35cca67c757acc0
kamakshikumari <kamakshi.kumari@successive.tech>
2020-11-09 12:06:54 +0530

    Processing

--- src/controller/user/routes.ts
+++ src/controller/user/routes.ts
@@ -1,25 +1,20 @@
 import * as express from 'express';
 import userController from './Controller';
+import { Router } from  'express';
 import validationHandler from '../../libs/validationHandler';
 import validation from './validation';
-import authMiddleware from '../../libs/routes/authMiddleWare';
-const UserRouter = express.Router();
-
+import authMiddleWare from '../../libs/routes/authMiddleWare';
+const UserRouter = Router();
 UserRouter.get('/', authMiddleWare('getUser1', 'read'), validationHandler(validation.get),
-    userController.get);
-
-UserRouter.post('/', authMiddleWare('getUser1', 'write'), validationHandler(validation.create),
-    userController.create);
-
-UserRouter.put('/', authMiddleWare('getUser1', 'write'), validationHandler(validation.update),
-    userController.update);
-
-UserRouter.delete('/:id', authMiddleWare('getUser1', 'delete'), validationHandler(validation.delete),
-    userController.delete);
-
-UserRouter.post('/login', validationHandler(validation.login), userController.login);
-
-UserRouter.get('/me', authMiddleWare('getUser1', 'all'), userController.me);
+    userController.get)
+    .post('/', authMiddleWare('getUser1', 'write'), validationHandler(validation.create),
+    userController.create)
+    .put('/', authMiddleWare('getUser1', 'write'), validationHandler(validation.update),
+    userController.update)
+    .delete('/', authMiddleWare('getUser1', 'delete'), validationHandler(validation.delete),
+    userController.delete)
+    .post('/login', validationHandler(validation.login), userController.login)
+    .get('/me', authMiddleWare('getUser1', 'all'), userController.me);
 
 
 export default UserRouter;
